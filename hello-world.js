ec4ca150fd6e847e62dc3e2831a6598ed403e9e5
Charles Gaudreau Jackson <gaudreaujacksoncharles@gmail.com>
2016-04-18 14:09:36 -0400

    exercice 1

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+// Create a file called hello-world.js . In it, write a simple node program that outputs "Hello World!" to the console.
+// Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the program was run.
+// Save, commit and push.
+
+function trailMessages() {
+  setTimeout(function() {
+    console.log("Hello World!");
+    setTimeout(function() {
+      console.log("Hello World Again!!");
+    },10000);
+  },0);
+}
